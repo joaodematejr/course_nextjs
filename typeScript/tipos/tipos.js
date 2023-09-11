c40831b03bdd1fdc10e2b55944977e65413b5a5b
joaodematejr <joaodematejr@gmail.com>
2023-09-11 02:48:28 -0300

    Feat(Tipos)

---
+++ typeScript/tipos/tipos.js
@@ -0,0 +1,19 @@
+"use strict";
+//string
+let nome = 'João';
+console.log(nome);
+// nome
+//numbers
+let idade = 27;
+idade = 27.5;
+console.log(idade);
+//boolean
+let possuiHobbies = false;
+console.log(possuiHobbies);
+// tipos explícitos
+let minhaIdade;
+minhaIdade = 27;
+console.log(typeof minhaIdade);
+// number
+//array
+let hobbies = ["Cozinhar", "Praticar Esportes"];
---
+++ typeScript/tipos/tipos.ts
@@ -0,0 +1,29 @@
+//string
+let nome = 'João'
+console.log(nome)
+// nome
+
+//numbers
+let idade = 27
+idade = 27.5
+console.log(idade)
+
+//boolean
+let possuiHobbies = false
+console.log(possuiHobbies)
+
+// tipos explícitos
+let minhaIdade: number
+minhaIdade = 27
+console.log(typeof minhaIdade)
+// number
+
+
+
+//array
+let hobbies: any[] = ["Cozinhar", "Praticar Esportes"]
+console.log(hobbies[0])
+console.log(typeof hobbies)
+// object
+
+hobbies = [100, 200, 300]
