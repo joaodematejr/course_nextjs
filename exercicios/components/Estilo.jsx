0fbaf8348d9bef9b58104dbe3c2afe08ae728ce6
joaodematejr <joao.demate@alquimidiacorp.com.br>
2023-09-19 13:38:54 -0300

    feat(styles)

---
+++ exercicios/components/Estilo.jsx
@@ -0,0 +1,15 @@
+export default function Estilo(props) {
+  return (
+    <div>
+      <h1
+        style={{
+          backgroundColor: props.numero >= 0 ? "#2D2" : "#D22",
+          color: props.color,
+          textAlign: props.direita ? "right" : "left",
+        }}
+      >
+        teste
+      </h1>
+    </div>
+  );
+}
---
+++ exercicios/pages/css/usandoEstilo.jsx
@@ -0,0 +1,11 @@
+
+import Estilo from "@/components/Estilo";
+
+export default function usandoEstilo() {
+  return (
+    <div>
+      <Estilo numero={-1} />
+      <Estilo numero={0} />
+    </div>
+  );
+}
