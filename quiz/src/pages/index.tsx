1bce80cca4f798f75ac28c9c564116e1ba5c7766
joaodematejr <joao.demate@alquimidiacorp.com.br>
2023-10-04 11:39:40 -0300

    fix && feat styles

--- quiz/src/pages/index.tsx
+++ quiz/src/pages/index.tsx
@@ -2,14 +2,26 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import Questao from "@/components/Questao";
 import RespostaModel from "@/model/resposta";
+import QuestaoModel from "@/model/questao";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const questaoTeste = new Questao(1, "Melhor cor?", [
+  const questaoTeste = new QuestaoModel(1, "Melhor cor?", [
     RespostaModel.errada("Vermelho"),
     RespostaModel.errada("Verde"),
     RespostaModel.certa("Azul"),
   ]);
-  return <Questao valor={questaoTeste} />;
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <Questao valor={questaoTeste} />
+    </div>
+  );
 }
