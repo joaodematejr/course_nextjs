c2ce3a0a2b4e7b608f13823431315a7ee5137fa2
joaodematejr <joao.demate@alquimidiacorp.com.br>
2023-10-04 14:33:43 -0300

    styles fix css

--- quiz/src/components/Questao.tsx
+++ quiz/src/components/Questao.tsx
@@ -1,6 +1,7 @@
 import QuestaoModel from "@/model/questao";
 import styles from "@/styles/Questao.module.css";
 import Enunciado from "./Enunciado";
+import Resposta from "./Resposta";
 
 interface QuestaoProps {
   valor: QuestaoModel;
@@ -9,9 +10,24 @@ interface QuestaoProps {
 export default function Questao(props: QuestaoProps) {
   const questao = props.valor;
 
+  function renderizarResposta() {
+    return questao.respostas.map((resposta, i) => {
+      return (
+        <Resposta
+          valor={resposta}
+          key={`${questao.id}-${i}`}
+          indice={i}
+          letra="A"
+          corFundoLetra="#F2C866"
+        />
+      );
+    });
+  }
+
   return (
     <div className={styles.questao}>
       <Enunciado texto={questao.enunciado} />
+      {renderizarResposta()}
     </div>
   );
 }
